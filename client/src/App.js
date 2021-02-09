b059f322af1af4f2ff236166f07387f02dfa9601
Philippe Gonse <pgonse@gmail.com>
2021-02-09 12:45:08 -0500

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetch('/express_backend')
+      .then(res => res.json())
+      .then(data => setData({ data: data.express}));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <p>{data ? data.data : "Loading..."}</p>
+    </div>
+  );
+}
+
+export default App;
