233bc4b07119aaa6690a11a8f75083cf76ad1068
Philippe Gonse <pgonse@gmail.com>
2021-02-22 10:59:34 -0500

    netlify deploy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Lisstening on port ${port}`));
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, './client/build')));
